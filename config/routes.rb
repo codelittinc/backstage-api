287205adf5647457bcedb2ad0fd751fc3bd79fdd
Kaio Magalhaes <me@kaiomagalhaes.com>
2023-09-30 10:09:07 -0300

    replace users/me with users/show

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+
   resources :projects
   resources :users
-  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   resources :customers
-  get 'users/me'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -5,35 +5,44 @@ require 'rails_helper'
 RSpec.describe UsersController, type: :controller do
   include_context 'authentication'
 
-  describe 'GET #me' do
+  describe 'GET #show' do
     context 'when user is logged in' do
-      it 'returns http success' do
-        get :me
-        expect(response).to have_http_status(:success)
+      context 'and id is me' do
+        it 'returns http success' do
+          get :show, params: { id: 'me' }
+          expect(response).to have_http_status(:success)
+        end
+
+        it 'returns the user data' do
+          get :show, params: { id: 'me' }
+          expect(response.parsed_body['email']).to eql(user_params[:user][:email])
+        end
       end
 
-      it 'returns the user data' do
-        get :me
-        expect(response.parsed_body['email']).to eql(user_params[:user][:email])
+      context "when the params are valid but the user doesn't exist" do
+        it 'creates a new user' do
+          expect do
+            get :show, params: { id: 'me' }
+          end.to change(User, :count).by(1)
+        end
       end
-    end
 
-    context "when the params are valid but the user doesn't exist" do
-      it 'creates a new user' do
-        expect do
-          get :me
-        end.to change(User, :count).by(1)
+      xcontext 'when user email is from a different domain than expected' do
+        it 'returns an unauthorized access' do
+          invalid_user_params = { user: FactoryBot.attributes_for(:user, email: 'codelitt@wrongdomain.con') }
+          invalid_user_params_base64 = Base64.encode64(invalid_user_params.to_json)
+
+          request.headers.merge!(Authorization: "Bearer #{invalid_user_params_base64}")
+          get :show, params: { id: 'me' }
+          expect(response).to have_http_status(:unauthorized)
+        end
       end
-    end
-
-    xcontext 'when user email is from a different domain than expected' do
-      it 'returns an unauthorized access' do
-        invalid_user_params = { user: FactoryBot.attributes_for(:user, email: 'codelitt@wrongdomain.con') }
-        invalid_user_params_base64 = Base64.encode64(invalid_user_params.to_json)
-
-        request.headers.merge!(Authorization: "Bearer #{invalid_user_params_base64}")
-        get :me
-        expect(response).to have_http_status(:unauthorized)
+      context 'and id is a number' do
+        it 'returns the user with that id' do
+          user = FactoryBot.create(:user)
+          get :show, params: { id: user.id }
+          expect(response.parsed_body['email']).to eql(user.email)
+        end
       end
     end
   end
