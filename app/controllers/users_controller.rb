9525e88e844be9971f7a0bf759b33d490617f9ff
Kaio Magalhaes <me@kaiomagalhaes.com>
2023-09-30 10:09:07 -0300

    replace users/me with users/show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,21 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
-  def me
-    render json: current_user
-  end
+  before_action :set_user, only: [:show]
 
   def index
     render json: User.all
+  end
+
+  def show
+    render json: @user
+  end
+
+  private
+
+  def set_user
+    id = params['id']
+
+    @user = id == 'me' ? current_user : User.find(id)
   end
 end
