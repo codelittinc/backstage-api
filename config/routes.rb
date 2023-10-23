b7cb34f61c45b481e2e6371471273fb8dd05b02f
Kaio Magalhaes <me@kaiomagalhaes.com>
2023-10-23 10:18:28 -0300

    update Procfile and seeds

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  mount RailsAdmin::Engine => "/admin", as: "rails_admin"
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
 
   resources :projects
   resources :users
