55f1ba19959eadb40ad08098cba1fbe2d40060d9
Kaio Magalhaes <me@kaiomagalhaes.com>
2023-10-24 12:28:52 -0300

    add data to issues and azure parser

---
+++ db/migrate/20231024135952_add_data_to_issues.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+class AddDataToIssues < ActiveRecord::Migration[7.0]
+  def change
+    # rubocop:disable Rails/BulkChangeTable
+    add_column :issues, :issue_id, :string
+    add_column :issues, :issue_type, :string
+    add_column :issues, :title, :string
+    # rubocop:enable Rails/BulkChangeTable
+  end
+end
