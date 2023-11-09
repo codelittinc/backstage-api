8d6768724355db0b1c7a9b7e135486f3a77f6632
Kaio Magalhaes <me@kaiomagalhaes.com>
2023-11-09 10:58:41 -0300

    improve pull prod backup script

--- bin/pull_prod_backup.sh
+++ bin/pull_prod_backup.sh
@@ -1,10 +1,12 @@
 rm latest.dump
+docker-compose down
 docker-compose up -d
-docker stop backstage-api
 heroku pg:backups:capture --app prod-backstage-api
 heroku pg:backups:download --app prod-backstage-api
 docker exec -it backstage-db psql -U postgres -c 'DROP DATABASE IF EXISTS backstage_development'
 docker exec -it backstage-db psql -U postgres -c "CREATE DATABASE backstage_development"
 docker exec -it backstage-db psql -U postgres backstage_development -c "CREATE SCHEMA IF NOT EXISTS heroku_ext AUTHORIZATION postgres"
 docker exec -it backstage-db pg_restore --no-owner  -U postgres -d backstage_development -1 ./app/latest.dump
+docker exec -it prod-backstage-api rails db:create
+docker exec -it prod-backstage-api rails db:migrate
 sh bin/dev
