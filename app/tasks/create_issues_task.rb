818de063496bcb71c44c986760f78b6d0e6b5650
Kaio Magalhaes <me@kaiomagalhaes.com>
2023-11-23 14:22:47 -0300

    fix issues task

--- app/tasks/create_issues_task.rb
+++ app/tasks/create_issues_task.rb
@@ -2,7 +2,8 @@
 
 class CreateIssuesTask
   def self.create!
-    projects = Project.active_on(Time.zone.today).with_ticket_system.where(sync_ticket_tracking_system: true)
+    projects = Project.active_in_period(Time.zone.today,
+                                        Time.zone.today).with_ticket_system.where(sync_ticket_tracking_system: true)
 
     projects.each do |project|
       ProjectIssuesWorker.perform_async(project.name)
