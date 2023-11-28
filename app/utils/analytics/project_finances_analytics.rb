cf894aa82fa77538a8e259c9406baa1cb8608c30
Kaio Magalhaes <me@kaiomagalhaes.com>
2023-11-28 13:22:16 -0300

    update company calculator to show sow names

--- app/utils/analytics/project_finances_analytics.rb
+++ app/utils/analytics/project_finances_analytics.rb
@@ -2,14 +2,14 @@
 
 module Analytics
   class ProjectFinancesAnalytics
-    def initialize(start_date, end_date, project)
+    def initialize(start_date, end_date, statement_of_work)
       @start_date = start_date.to_datetime.beginning_of_day
       @end_date = end_date.to_datetime.end_of_day
-      @project = project
+      @statement_of_work = statement_of_work
     end
 
     def data
-      Analytics::Finances::ProjectCalculator.new(@project, @start_date, @end_date).data
+      Analytics::Finances::ProjectCalculator.new(@statement_of_work, @start_date, @end_date).data
     end
   end
 end
