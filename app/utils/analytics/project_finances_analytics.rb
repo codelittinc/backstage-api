32e2c9c968fbf19356ec7fee0936d99ab1025d43
Kaio Magalhaes <me@kaiomagalhaes.com>
2023-12-12 10:45:43 -0300

    fix project finances view

--- app/utils/analytics/project_finances_analytics.rb
+++ app/utils/analytics/project_finances_analytics.rb
@@ -2,14 +2,14 @@
 
 module Analytics
   class ProjectFinancesAnalytics
-    def initialize(start_date, end_date, statement_of_work)
+    def initialize(start_date, end_date, project)
       @start_date = start_date.to_datetime.beginning_of_day
       @end_date = end_date.to_datetime.end_of_day
-      @statement_of_work = statement_of_work
+      @project = project
     end
 
     def data
-      Analytics::Finances::ProjectCalculator.new(@statement_of_work, @start_date, @end_date).data
+      Analytics::Finances::ProjectCalculator.new(@project, @start_date, @end_date).data
     end
   end
 end
