f6fcd66339088bd12d5ee49c69cbe05856961ae0
Kaio Magalhaes <me@kaiomagalhaes.com>
2023-12-13 20:05:35 -0300

    fix calculations for the statements of work

--- app/controllers/analytics/finances_controller.rb
+++ app/controllers/analytics/finances_controller.rb
@@ -3,16 +3,21 @@
 module Analytics
   class FinancesController < ApplicationController
     before_action :set_project, only: %i[index]
+    before_action :set_statement_of_works, only: %i[index]
 
     def index
-      @finances = if @project
-                    Analytics::Finances::Models::FinancialStatementsOfWork.new(@project, start_date, end_date)
-                  else
-                    Analytics::Finances::Models::FinancialProjects.new(start_date, end_date)
-                  end
+      @finances = Analytics::Finances::Models::FinancialStatementsOfWork.new(@statement_of_works, start_date, end_date,
+                                                                             false)
+      @finances.financial_items = @finances.financial_items.sort_by(&:name)
     end
 
     private
+
+    def set_statement_of_works
+      @statement_of_works = @project.statement_of_works.active_in_period(start_date, end_date) if @project
+
+      @statement_of_works = StatementOfWork.active_in_period(start_date, end_date)
+    end
 
     def set_project
       @project = Project.where(id: params[:project_id]).first
