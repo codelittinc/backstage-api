0ffef3d203bdb9a15d8d2457c8021bd20aec1e07
Kaio Magalhaes <me@kaiomagalhaes.com>
2023-12-20 17:22:59 -0300

    fix retainer calculation

--- app/models/retainer_contract_model.rb
+++ app/models/retainer_contract_model.rb
@@ -15,6 +15,16 @@ class RetainerContractModel < ApplicationRecord
 
   has_one :statement_of_work, as: :contract_model, dependent: :destroy
 
+  def expected_income(start_date, end_date)
+    revenue_per_period * months_in_period(start_date, end_date)
+  end
+
+  def months_in_period(start_date, end_date)
+    return 1 if start_date.month == end_date.month && start_date.year == end_date.year
+
+    ((end_date.year * 12) + end_date.month) - ((start_date.year * 12) + start_date.month)
+  end
+
   def assignment_executed_income(_assignment, _start_date, _end_date)
     0
   end
