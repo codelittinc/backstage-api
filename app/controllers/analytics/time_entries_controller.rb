76926ff780b32aec1ca1d7f8dbd0322123894734
Kaio Magalhaes <me@kaiomagalhaes.com>
2024-03-14 14:16:41 -0300

    make time entries available per sow

--- app/controllers/analytics/time_entries_controller.rb
+++ app/controllers/analytics/time_entries_controller.rb
@@ -3,9 +3,10 @@
 module Analytics
   class TimeEntriesController < ApplicationController
     before_action :set_project, only: %i[index]
+    before_action :set_statement_of_work, only: %i[index]
 
     def index
-      @time_entries = Analytics::TimeEntriesAnalytics.new(@project, params[:start_date],
+      @time_entries = Analytics::TimeEntriesAnalytics.new(@project, @statement_of_work, params[:start_date],
                                                           params[:end_date]).data
 
       render json: @time_entries
@@ -15,6 +16,10 @@ module Analytics
 
     def set_project
       @project = Project.where(id: params[:project_id]).first
+    end
+
+    def set_statement_of_work
+      @statement_of_work = StatementOfWork.where(id: params[:statement_of_work_id]).first
     end
   end
 end
